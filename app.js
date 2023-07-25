dcc8133d12a95458603c6b3fd94cdab36aa9f1ca
cadeci <ciaracade03@gmail.com>
2023-07-25 01:44:11 -0400

    Fully functional

--- app.js
+++ app.js
@@ -2,18 +2,44 @@
 const canvas = document.querySelector("#canvas");
 const inputSlider = document.querySelector("#slider");
 const sliderValue = document.querySelector("#canvasSize");
+const blackBtn = document.querySelector("#black");
+const rainbowBtn = document.querySelector("#rainbow");
+const eraserBtn = document.querySelector("#eraser");
+const clearBtn = document.querySelector("#clear");
 
-
-// Set vars and constants
+// Set let, vars, and constants
 const defaultCanvasPixelSize = 16;
 const canvasSize = 500;
+let currentColor = "black";
+let currentCanvasSize = 16;
+
 
 // Event for slider
 sliderValue.textContent = inputSlider.value + "x" + inputSlider.value;
 inputSlider.addEventListener("input", () => {
   sliderValue.textContent = inputSlider.value + "x" + inputSlider.value;
   setCanvas(inputSlider.value);
+  currentCanvasSize = inputSlider.value;
 });
+
+// Event for buttons
+blackBtn.addEventListener("click", () => {
+  currentColor = "black";
+  setCanvas(currentCanvasSize);
+})
+
+rainbowBtn.addEventListener("click", () => {
+  currentColor = "rainbow";
+  setCanvas(currentCanvasSize);
+})
+
+eraserBtn.addEventListener("click", () => {
+  currentColor = "eraser";
+})
+
+clearBtn.addEventListener("click", () => {
+   setCanvas(currentCanvasSize);
+})
 
 //Sets canvas to default size
 setCanvas(defaultCanvasPixelSize);
@@ -28,8 +54,8 @@ function setCanvas(size) {
     }
   }
 
-  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   for (let i = 0; i < size * size; i++) {
     const pixel = document.createElement("div");
@@ -47,12 +73,13 @@ function randomColor(pixel) {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
+
+  if(currentColor === "black"){
+    pixel.target.style.backgroundColor = "rgb(0, 0, 0)";
+  } else if (currentColor === "rainbow"){
   pixel.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  console.log(`Hello ${red}`);
+  } else if (currentColor === "eraser"){
+    pixel.target.style.backgroundColor = "rgb(255, 255, 255)";
+  }
 }
 
-// Event for mouse hover
-
-
-
-// Changes color depending on color setting
